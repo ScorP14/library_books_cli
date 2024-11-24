62b6308e45bf3448c3d570f56cda3d89cc113fad
ScorP14 <mail@mail.com>
2024-11-24 11:17:09 +0800

    edit book repository, add contextmanager

--- src/infrastructure/repository/books/json_repo.py
+++ src/infrastructure/repository/books/json_repo.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from dataclasses import dataclass
 from typing import Sequence, Any
 import json
@@ -31,61 +32,59 @@ class BookRepositoryJson(IBookRepository):
 
     def add(self, event: CreateBook) -> Book:
         """Добавить книгу в репозиторий"""
-        database: JsonDTO = self._load_books()
+        with self.connect(save=True) as database:
+            _id = database.metadata["autoincrement"]
+            database.data[_id] = event.as_dict()
+            database.metadata["autoincrement"] += 1
 
-        _id = database.metadata["autoincrement"]
-        database.data[_id] = event.as_dict()
-        database.metadata["autoincrement"] += 1
-
-        self._save_books(database)
         book = MapperBook.from_dict_to_model({"id": _id} | event.as_dict())
         return book
 
     def delete(self, event: DeleteBook) -> None:
         """Удалить книгу из репозитория."""
-        database: JsonDTO = self._load_books()
-
-        str_id = str(event.id)
-        if str_id not in database.data:
-            raise NotFoundByIdError(event.id)
-        del database.data[str_id]
-
-        self._save_books(database)
+        with self.connect(save=True) as database:
+            str_id = str(event.id)
+            if str_id not in database.data:
+                raise NotFoundByIdError(event.id)
+            del database.data[str_id]
 
     def search(self, query: str) -> Sequence[Book] | None:
         """Поиск книги в репозитории. Вернуть массив книг или ничего."""
-        database: JsonDTO = self._load_books()
-
-        result = []
-        for _id, book in database.data.items():
-            if (
-                book["title"] == query
-                or book["author"] == query
-                or (query.isdigit() and book["year"] == int(query))
-            ):
-                result.append(MapperBook.from_dict_to_model({"id": _id} | book))
-        return result
+        with self.connect(save=False) as database:
+            result = []
+            for _id, book in database.data.items():
+                if (
+                    book["title"] == query
+                    or book["author"] == query
+                    or (query.isdigit() and book["year"] == int(query))
+                ):
+                    result.append(MapperBook.from_dict_to_model({"id": _id} | book))
+            return result
 
     def all(self) -> Sequence[Book]:
         """Получить все книги из репозитория."""
-        database: JsonDTO = self._load_books()
-
-        return [
-            MapperBook.from_dict_to_model({"id": _id} | book)
-            for _id, book in database.data.items()
-        ]
+        # database: JsonDTO = self._load_books()
+        with self.connect(save=False) as database:
+            return [
+                MapperBook.from_dict_to_model({"id": _id} | book)
+                for _id, book in database.data.items()
+            ]
 
     def set_status(self, event: SetStatusBook) -> None:
         """Изменить статус книги."""
+        with self.connect(save=True) as database:
+            str_id = str(event.id)
+            if str_id not in database.data:
+                raise NotFoundByIdError(event.id)
+
+            database.data[str_id]["status"] = event.status.value
+
+    @contextmanager
+    def connect(self, save: bool = False):
         database: JsonDTO = self._load_books()
-
-        str_id = str(event.id)
-        if str_id not in database.data:
-            raise NotFoundByIdError(event.id)
-
-        database.data[str_id]["status"] = event.status.value
-
-        self._save_books(database)
+        yield database
+        if save:
+            self._save_books(database)
 
     def _save_books(self, data: JsonDTO):
         with open(self.path_to_file, "w", encoding="utf-8") as file:
